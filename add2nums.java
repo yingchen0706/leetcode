0145eadf2a23750dfe15ad9dd57314b508b0ac77
yingchen0706 <yingchen0706@users.noreply.github.com>
2018-06-07 01:02:25 +0800

    Update add2nums.java

--- add2nums.java
+++ add2nums.java
@@ -7,51 +7,24 @@
  * }
  */
 class Solution {
-    private int getLen(ListNode l) {
-        int res = 0;
-        while(l != null) {
-            l=l.next;
-            res++;
-        }
-        return res;
-    } 
-    private ListNode getTail(ListNode l) {
-        if (l== null) {
-            return l;
-        }
-        while (l.next!=null) {
-            l = l.next;
-        }
-        return l;
-    }
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int a = getLen(l1);
-        int b = getLen(l2);
+        ListNode p1 = l1;
+        ListNode p2 = l2;
         ListNode root = null;
         ListNode tailR = root;
         
-        int diff = Math.abs(a-b);
-        if (diff != 0) {
-            ListNode head = new ListNode(0);
-            int cnt = diff - 1;
-            ListNode tail = head;
-            while (cnt > 0) {
-                tail.next = new ListNode(0);
-                tail = tail.next;
-                cnt--;
-            }
-            if (a>b) {
-                getTail(l2).next = head;
-            } else {
-                getTail(l1).next = head;
-            }
-        }
         int add = 0;
-        while (l1 != null) {
-            ListNode node = new ListNode((l1.val+ l2.val + add)%10);
-            add = ((l1.val + l2.val + add) >=10) ? 1:0;
-            l1 = l1.next;
-            l2 = l2.next;
+        while (p1!=null || p2!=null) {
+            int x = p1 !=null?p1.val:0;
+            int y = p2 !=null?p2.val:0;
+            ListNode node = new ListNode((x+y + add)%10);
+            add = ((x+y + add) >=10) ? 1:0;
+            if (p1!=null) {
+                p1 = p1.next;
+            }
+            if (p2!=null) {
+                p2 = p2.next;
+            }
             if (root == null) {
                 root = node;
                 tailR = root;
