0611415cbc06f7e1795301d4275bdf5600ad9a47
yingchen0706 <yingchen0706@users.noreply.github.com>
2018-06-27 00:46:07 +0800

    Create findduplicate2.java

---
+++ findduplicate2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+            
+        int ptr1 = nums[0];
+        int ptr2 = slow;
+        while (ptr1 != ptr2) {
+            ptr1 = nums[ptr1];
+            ptr2 = nums[ptr2];
+        }
+        
+        return ptr1;
+    }
+}
