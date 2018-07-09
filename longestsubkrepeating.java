a1c453639390c854243c263321bedf6ae51afcb0
yingchen0706 <yingchen0706@users.noreply.github.com>
2018-07-10 00:20:02 +0800

    Create longestsubkrepeating.java

---
+++ longestsubkrepeating.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int longestSubstring(String s, int k) {
+        char[] str = s.toCharArray();
+        return helper(str, 0, str.length, k);
+    }
+    public int helper(char[] str, int start, int end, int k) {
+        if (end - start < k) {
+            return 0;
+        }
+        int[] count = new int[26];
+        for (int i = start; i<end; i++) {
+            count[str[i] - 'a']++;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (count[i] < k && count[i] > 0) {
+                for (int j = start; j < end; j++) {
+                    if (str[j] == i+'a') {
+                        int left = helper(str, start, j,k);
+                        int right = helper(str, j+1, end, k);
+                        return Math.max(left, right);
+                    }
+                }
+            }
+        }
+        return end-start;
+    }
+}
